96785af1910a8992195d4fb2a02cc51d2ea8e933
Neha Kumari <96020484+nehakumari2003@users.noreply.github.com>
2024-01-01 19:22:21 +0530

    Add files via upload

---
+++ PowerOfTwo.cpp
@@ -0,0 +1,20 @@
+/*Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+An integer n is a power of two, if there exists an integer x such that n == 2x.*/
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        int ans=1;
+        for(int i=0; i<=30; i++){
+            if(ans==n)
+            {
+                return true;
+            }
+            if(ans < INT_MAX/2)
+            ans = ans*2;
+        }
+        return false;
+        
+    }
+};
