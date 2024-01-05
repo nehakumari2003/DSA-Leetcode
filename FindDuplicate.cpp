f22a11e9f80314cd8272bf390e9e67dac374d91f
Neha Kumari <96020484+nehakumari2003@users.noreply.github.com>
2024-01-05 22:13:51 +0530

    Add files via upload

---
+++ FindDuplicate.cpp
@@ -0,0 +1,21 @@
+/*Problem statement
+You are given an array ‘ARR’ of size ‘N’ containing each number between 1 and ‘N’ - 1 at least once. There is a single integer value that is present in the array twice. Your task is to find the duplicate integer value present in the array.
+
+For example:
+
+Consider ARR = [1, 2, 3, 4, 4], the duplicate integer value present in the array is 4. Hence, the answer is 4 in this case.
+Note :
+A duplicate number is always present in the given array.*/
+
+int findDuplicate(vector<int> &arr) 
+{
+   int ans=0;
+   for(int i=0 ; i< arr.size(); i++){
+       ans=ans^arr[i];
+   }
+   for(int i=1; i<arr.size(); i++){
+       ans=ans^i;
+   }
+   return ans;
+	
+}
