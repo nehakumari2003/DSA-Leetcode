c59d4bf13c6514d31d6a872a7ae01d598110a6d8
Neha Kumari <96020484+nehakumari2003@users.noreply.github.com>
2024-01-08 21:13:25 +0530

    Add files via upload

---
+++ Pivot.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int getPivot(int arr[], int n){
+    int s = 0, e = n-1;
+    int mid = s+(e-s)/2;
+    while(s<e){
+        if(arr[mid]>=arr[0]){
+            s=mid+1;
+        }
+        else{
+            e=mid;
+        }
+        mid=s+(e-s)/2;
+    }
+    return s; //we can return s or e
+
+} 
+int main(){
+    int arr[5]={3,8,10,17,1};
+    cout<<"Pivot is : "<< getPivot(arr,5)<<endl;
+}
