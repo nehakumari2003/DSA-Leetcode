a26fb397919439cb15c6b0687eae15d878ddda47
Neha Kumari <96020484+nehakumari2003@users.noreply.github.com>
2024-01-18 21:16:38 +0530

    Add files via upload

---
+++ RemoveconsecutiveDuolicate.cpp
@@ -0,0 +1,21 @@
+/*You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them.
+
+We repeatedly make duplicate removals on s until we no longer can.
+
+Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
+
+ */ 
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        int i = 0, n = s.length();
+        for (int j = 0; j < n; ++j, ++i) {
+            s[i] = s[j];
+            if (i > 0 && s[i - 1] == s[i]) 
+                i -= 2;
+        }
+        return s.substr(0, i);
+    }
+        
+    
+};
