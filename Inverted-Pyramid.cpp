9971a3f8f0dca77021ddc0d083acde26219c8aea
Neha Kumari <96020484+nehakumari2003@users.noreply.github.com>
2024-03-02 12:56:14 +0530

    Add files via upload

---
+++ Inverted-Pyramid.cpp
@@ -0,0 +1,20 @@
+// C++ Program to print inverted pyramid 
+#include <iostream> 
+using namespace std; 
+
+int main() 
+{ 
+	int rows = 5; 
+
+	for (int i = rows; i >= 1; i--) { 
+		for (int j = rows; j > i; j--) { 
+			cout << " "; 
+		} 
+		for (int k = 1; k <= (2 * i - 1); k++) { 
+			cout << "*"; 
+		} 
+		cout << endl; 
+	} 
+
+	return 0; 
+}
