2ab457f80d1b92645c647395248dd663bf893be5
Neha Kumari <96020484+nehakumari2003@users.noreply.github.com>
2024-04-25 19:33:18 +0530

    Add files via upload

---
+++ Search in a 2-D matrix.cpp
@@ -0,0 +1,31 @@
+/*Problem statement
+You have been given a 2-D array 'mat' of size 'M x N' where 'M' and 'N' denote the number of rows and columns, respectively. The elements of each row are sorted in non-decreasing order.
+Moreover, the first element of a row is greater than the last element of the previous row (if it exists).
+You are given an integer ‘target’, and your task is to find if it exists in the given 'mat' or not.*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+
+    
+    int low = 0, high = n * m - 1;
+     if (n == 0) return false;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int row = mid / m, col = mid % m;
+        if (matrix[row][col] == target) return true;
+        else if (matrix[row][col] < target) low = mid + 1;
+        else high = mid - 1;
+    }
+    return false;
+}
+
+int main()
+{
+    vector<vector<int>> matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+    searchMatrix(matrix, 8) == true ? cout << "true\n" : cout << "false\n";
+}
+
