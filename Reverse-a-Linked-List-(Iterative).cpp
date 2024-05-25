019baa4015e2578e29c3b99108396e3416b6e166
Neha Kumari <96020484+nehakumari2003@users.noreply.github.com>
2024-05-25 12:38:17 +0530

    Add files via upload

---
+++ Reverse-a-Linked-List-(Iterative).cpp
@@ -0,0 +1,78 @@
+
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+public:
+    
+    int data;   
+    
+    Node* next;      
+
+    Node(int data1, Node* next1) {
+        data = data1;
+        next = next1;
+    }
+
+    Node(int data1) {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node* reverseLinkedList(Node *head)
+{
+   
+   Node* temp = head;  
+   
+   Node* prev = NULL;  
+   
+   while(temp != NULL){  
+       
+       Node* front = temp->next;  
+       
+    
+       temp->next = prev;  
+       
+       prev = temp;  
+       
+       temp = front; 
+   }
+   
+   
+   return prev;  
+}
+
+
+void printLinkedList(Node* head) {
+    Node* temp = head;
+    while (temp != nullptr) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    
+    Node* head = new Node(1);
+    head->next = new Node(3);
+    head->next->next = new Node(2);
+    head->next->next->next = new Node(4);
+
+    
+    cout << "Original Linked List: ";
+    printLinkedList(head);
+
+    head = reverseLinkedList(head);
+
+    cout << "Reversed Linked List: ";
+    printLinkedList(head);
+
+    return 0;
+}
+
+
