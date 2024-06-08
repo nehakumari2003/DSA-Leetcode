2138ce10ee14af38d012c104713abc6dcf3f8e8f
Neha Kumari <96020484+nehakumari2003@users.noreply.github.com>
2024-06-08 20:48:27 +0530

    Add files via upload

---
+++ Sort-Linked-List.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+ 
+struct Node 
+{ 
+	Node* next; 
+	int data; 
+}; 
+
+void push(Node** head, int data) 
+{ 
+	Node* newNode = new Node; 
+	newNode->next = (*head); 
+	newNode->data = data; 
+	(*head) = newNode; 
+} 
+
+
+void printList(Node* head) 
+{ 
+	while (head != NULL) 
+	{ 
+		cout << head->data; 
+		if (head->next != NULL) 
+			cout << " -> "; 
+		head = head->next; 
+	} 
+	cout<<endl; 
+} 
+
+void sortList(Node** head) 
+{ 
+	
+	Node* prev = (*head); 
+	Node* curr = (*head)->next; 
+
+	// Traverse list 
+	while (curr != NULL) 
+	{ 
+
+		if (curr->data < prev->data) 
+		{ 
+
+			prev->next = curr->next; 
+
+			curr->next = (*head); 
+			(*head) = curr; 
+
+			curr = prev; 
+		} 
+
+		else
+			prev = curr; 
+
+		// Move current 
+		curr = curr->next; 
+	} 
+} 
+
+int main() 
+{ 
+	Node* head = NULL; 
+	push(&head, -5); 
+	push(&head, 5); 
+	push(&head, 4); 
+	push(&head, 3); 
+	push(&head, -2); 
+	push(&head, 1); 
+	push(&head, 0); 
+
+	cout << "Original list :"; 
+	printList(head); 
+
+	sortList(&head); 
+
+	cout << "Sorted list :"; 
+	printList(head); 
+
+	return 0; 
+} 
