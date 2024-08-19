68c9ac6e1876c1600fa0354f2fca2efb63d8b19b
Neha Kumari <96020484+nehakumari2003@users.noreply.github.com>
2024-08-19 22:29:49 +0530

    Add files via upload

---
+++ Implement stack using array.cpp
@@ -0,0 +1,44 @@
+/*Write a program to implement a Stack using Array. Your task is to use the class as shown in the comments in the code editor and complete the functions push() and pop() to implement a stack. The push() method takes one argument, an integer 'x' to be pushed into the stack and pop() which returns an integer present at the top and popped out from the stack. If the stack is empty then return -1 from the pop() method.*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+class Stack {
+  int size;
+  int * arr;
+  int top;
+  public:
+    Stack() {
+      top = -1;
+      size = 1000;
+      arr = new int[size];
+    }
+  void push(int x) {
+    top++;
+    arr[top] = x;
+  }
+  int pop() {
+    int x = arr[top];
+    top--;
+    return x;
+  }
+  int Top() {
+    return arr[top];
+  }
+  int Size() {
+    return top + 1;
+  }
+};
+int main() {
+
+  Stack s;
+  s.push(6);
+  s.push(3);
+  s.push(7);
+  cout << "Top of stack is before deleting any element " << s.Top() << endl;
+  cout << "Size of stack before deleting any element " << s.Size() << endl;
+  cout << "The element deleted is " << s.pop() << endl;
+  cout << "Size of stack after deleting an element " << s.Size() << endl;
+  cout << "Top of stack after deleting an element " << s.Top() << endl;
+  return 0;
+}
