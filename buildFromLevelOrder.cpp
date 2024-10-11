92d45303276d607c1c5b3b0282b6da2d7125028e
Neha Kumari <96020484+nehakumari2003@users.noreply.github.com>
2024-10-11 23:53:49 +0530

    Add files via upload

---
+++ buildFromLevelOrder.cpp
@@ -0,0 +1,104 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+class node {
+    public:
+        int data;
+        node* left;
+        node* right;
+
+    node(int d) {
+        this -> data = d;
+        this -> left = NULL;
+        this -> right = NULL;
+    }
+};
+
+node* buildTree(node* root){
+    cout << "Enter the data: " << endl;
+    int data;
+    cin >> data;
+    root = new node(data);    
+
+    if(data == -1) {
+        return NULL;
+    }
+
+    cout << "Enter data for inserting in left of " << data << endl;
+    root->left = buildTree(root->left);
+    cout << "Enter data for inserting in right of " << data << endl;
+    root->right = buildTree(root->right);
+    return root;
+
+}
+void levelOrderTraversal(node* root) {
+    queue<node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()) {
+        node* temp = q.front();
+        q.pop();
+
+        if(temp == NULL) { 
+            
+            cout << endl;
+            if(!q.empty()) { 
+                
+                q.push(NULL);
+            }  
+        }
+        else{
+            cout << temp -> data << " ";
+            if(temp ->left) {
+                q.push(temp ->left);
+            }
+
+            if(temp ->right) {
+                q.push(temp ->right);
+            }
+        }
+    }
+
+}
+void buildFromLevelOrder(node* &root){
+    queue<node*> q;
+    cout<<"enter data for root" <<endl;
+    int data;
+    cin >> data;
+    root = new node(data);
+
+    q.push(root);
+
+    while(!q.empty()){
+        node* temp = q.front();
+        q.pop();
+
+        cout<<"enter left node for : " << temp ->data <<endl;
+        int leftData;
+        cin >> leftData;
+
+        if(leftData != -1){
+            temp -> left = new node(leftData);
+            q.push(temp -> left);
+        }
+        cout << "Enter right node for: " << temp->data << endl;
+        int rightData;
+        cin >> rightData;
+
+        if(rightData != -1) {
+            temp -> right = new node(rightData);
+            q.push(temp->right);
+        }
+    }
+}
+int main() {
+
+    node* root = NULL;
+
+    buildFromLevelOrder(root);
+    levelOrderTraversal(root);
+    // 1 3 5 7 11 17 -1 -1 -1 -1 -1 -1 -1
+    return 0;
+}
