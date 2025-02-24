66bfe5c0b5fd8953bb93fdab1631dead23d036eb
Neha Kumari <96020484+nehakumari2003@users.noreply.github.com>
2025-02-24 23:41:03 +0530

    Add files via upload

---
+++ Find Second Largest Number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int secondLargest(int arr[], int n) {
+    int first = INT_MIN, second = INT_MIN;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > first) {
+            second = first;
+            first = arr[i];
+        } else if (arr[i] > second && arr[i] != first) {
+            second = arr[i];
+        }
+    }
+    return second;
+}
+
+int main() {
+    int arr[] = {12, 35, 1, 10, 34, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Second Largest: " << secondLargest(arr, n);
+}
